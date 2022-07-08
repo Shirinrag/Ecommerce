549234c042bbeab623f850920180436c58701fe5
Shirin <shirin@stzsoft.com>
2022-07-08 19:24:50 +0530

    changes in app

--- application/views/admin/add_banner.php
+++ application/views/admin/add_banner.php
@@ -8,6 +8,7 @@
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" type="text/css" id="theme" href="<?php echo base_url()?>assets-admin/css/theme-default.css"/>
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
         <style type="text/css">
             .form-horizontal .form-group {
     margin-right: 0px;
