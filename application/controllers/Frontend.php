1d003b1201e84e556bfa374cf02fc951db5ff5b6
Mansipalkar019 <108782007+Mansipalkar019@users.noreply.github.com>
2022-07-08 16:59:07 +0530

    web

--- application/controllers/Frontend.php
+++ application/controllers/Frontend.php
@@ -5,11 +5,14 @@ class Frontend extends CI_Controller {
 
 	public function index()
 	{
-		$data=array('fk_lang_id'=>1);
-		$response=$this->link->hits('get-home-page-data',$data);
-		print_r($response);die;
-		$data['topbanner']=json_decode($response,TRUE);
-		print_r($data['topbanner']);die();
+
+		
+		// $data=array('fk_lang_id'=>1);
+		
+		// $response=$this->link->hits('get-home-page-data',$data);
+		// print_r($response);die;
+		// $data['topbanner']=json_decode($response,TRUE);
+		// print_r($data['topbanner']);die();
 		$this->load->view('frontend/home');
 	}
 
